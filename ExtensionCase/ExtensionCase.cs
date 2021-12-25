60f5528a00bdb23437b8af9074f24af6381f546c
SuperDMWebDev <hadtnt71@gmail.com>
2021-12-25 20:36:56 +0700

    Them extension case

---
+++ ExtensionCase/ExtensionCase.cs
@@ -0,0 +1,51 @@
+﻿using RenameRules;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace ExtensionCase
+{
+    public class ExtensionCase : IRenameRule
+    {
+        public string _Extension { get; set; }
+
+        public ExtensionCase()
+        {
+            _Extension = "";
+        }
+        public ExtensionCase(string origin)
+        {
+            _Extension = origin;
+        }
+        public string getExtension()
+        {
+            return _Extension;
+        }
+
+        public string Name => "ExtensionCase";
+
+        public IRenameRule Clone()
+        {
+            return new ExtensionCase(_Extension);
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public string Process(string origin)
+        {
+            string fileName = Path.GetFileNameWithoutExtension(origin);
+
+            string res = fileName + "." + _Extension;
+            return res;
+        }
+
+        IRenameRule IRenameRule.Parse(string origin)
+        {
+
+            return new ExtensionCase(origin);
+
+        }
+    }
+}
